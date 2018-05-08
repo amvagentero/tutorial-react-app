0df6a38182c3e9549f8314162ad0256d9fb63dac
Alejandro MdeV <alejandroid.dev@gmail.com>
2018-05-08 16:30:32 +0200

    react hello world

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,27 @@
+const path = require('path');
+
 module.exports = options => {
   return {
     entry: './index.js',
     output: {
       filename: 'bundle.js',
+      path: path.resolve(__dirname, 'app')
+    },
+    module: {
+      rules: [
+        {
+          test: /.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                cacheDirectory: true,
+              },
+            },
+          ],
+        },
+      ],
     },
   }
 };
